e7a43b73ba067cf1da3eab4035f99acc53b271da
Kishor C <kishor1124@gmail.com>
2025-10-29 12:58:30 +0530

    jdbc examples

--- examples/java-examples/java-collections-wrappers/src/com/cisco/Employee.java
+++ examples/java-examples/java-collections-wrappers/src/com/cisco/Employee.java
@@ -1,6 +1,7 @@
 package com.cisco;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Employee {
 	private int id;
@@ -14,6 +15,23 @@ public class Employee {
 		this.name = name;
 		this.dob = dob;
 		this.salary = salary;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return id == other.id;
 	}
 
 	@Override
