381f81ae79957509ef3c296361ce3d1351770120
Kishor C <kishor1124@gmail.com>
2025-10-31 15:57:09 +0530

    servlet examples

---
+++ examples/java-examples/java-servlets-intermediate/src/main/java/com/cisco/FilterDemo2.java
@@ -0,0 +1,60 @@
+package com.cisco;
+
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
+
+/**
+ * Servlet Filter implementation class FilterDemo2
+ */
+@WebFilter(dispatcherTypes = {DispatcherType.FORWARD }
+					, urlPatterns = { "/FilterDemo2" }, servletNames = { "SimpleServlet" })
+public class FilterDemo2 extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public FilterDemo2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+				// place your code here
+				LocalTime time1 = LocalTime.now();
+				System.out.println("before doFilter");
+				// pass the request along the filter chain
+				chain.doFilter(request, response); // this sends request to the servlet & gets the response
+				LocalTime time2 = LocalTime.now();
+				System.out.println("after doFilter: ");
+				System.out.println("Time taken: "+Duration.between(time1, time2));
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
